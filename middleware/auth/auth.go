4a0ebf287af691a89a007608ee5c757655963a84
Rafal Leszko <rafal@livepeer.org>
2021-12-03 15:43:10 +0100

    Remove authorization

--- middleware/auth/auth.go
+++ middleware/auth/auth.go
@@ -1,14 +1,8 @@
 package auth
 
-import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/hex"
-	"os"
-)
-
 func IsAuthorized(authHeader string, body []byte) bool {
-	hash := hmac.New(sha256.New, []byte(os.Getenv("SECRET")))
-	hash.Write(body)
-	return hex.EncodeToString(hash.Sum(nil)) == authHeader
+	return true
+	//hash := hmac.New(sha256.New, []byte(os.Getenv("SECRET")))
+	//hash.Write(body)
+	//return hex.EncodeToString(hash.Sum(nil)) == authHeader
 }
